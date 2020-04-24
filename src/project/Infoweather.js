e5d584151b422823576379dfaefda44b7b21fd24
Mahmoud Badran <mahmoudbd@gmail.com>
2020-04-24 03:38:07 +0200

    Hackyourweather-w3

---
+++ src/project/Infoweather.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Infoweather = ({ props, deleteItem }) => {
+	const kelvinToCelcius = (kelvin) => {
+		return (kelvin - 273).toFixed(2);
+	};
+	return (
+		<div className="main">
+			<h3>
+				{props.name} {props.sys.country}
+			</h3>
+			<button className="deleteBtn" onClick={() => deleteItem(props.id)}>
+				X
+			</button>
+			<div className="weatherMain">
+				<h5>{props.weather[0].main}</h5>
+				<p> {props.weather[0].description} </p>
+			</div>
+
+			<p> min temp: {kelvinToCelcius(props.main.temp_min)} °C </p>
+			<p> max temp: {kelvinToCelcius(props.main.temp_max)} °C </p>
+			<p>
+				location: {props.coord.lat} {props.coord.lon}
+			</p>
+		</div>
+	);
+};
+
+export default Infoweather;
